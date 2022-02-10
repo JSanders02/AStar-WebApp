b48935ae513ab93f9847ed450c8e007b61be612e
Jack Sanders <jsanders4@sheffield.ac.uk>
2022-02-10 00:39:27 +0000

    Added functionality to set start/finish nodes

--- js/AStar.js
+++ js/AStar.js
@@ -1,11 +1,28 @@
-let globals = {
-    searchDiv: document.getElementById("search-area"),
-    clearButton: document.getElementById("clear-map"),
-    rowNum: 20,
-    colNum: 20,
-    // Use a getter as this.colNum does not work directly inside an object literal.
-    get boxSize() {
-        return 100 / this.colNum;
+
+ const searchDiv = document.getElementById("search-area");
+ const clearButton = document.getElementById("clear-map");
+ const startButton = document.getElementById("start-search");
+ const instructions = document.getElementById("instructions");
+
+ const stages = [setWall, setStart, setFinish];
+ const text = ["Draw a map", "Select start node", "Select finish node"];
+ let currentStage = 0;
+
+ const rowNum = 20;
+ const colNum = 20;
+ const boxSize = 100 / colNum;
+
+function advanceStage() {
+    currentStage++;
+
+    instructions.innerText = text[currentStage];
+
+    clearButton.classList.add("hidden");
+    startButton.classList.add("hidden");
+
+    for (let node of searchDiv.children) {
+        node.removeEventListener("click", stages[currentStage-1]);
+        node.addEventListener("click", stages[currentStage], false);
     }
 }
 
@@ -30,33 +47,54 @@ function clearWall(event) {
     return false
 }
 
+function setFinish(event) {
+    // Select node that was clicked
+    let currentNode = event.currentTarget;
+
+    if (currentNode.dataset.wall !== "true" && currentNode.dataset.start !== "true") {
+        currentNode.style.backgroundColor = "red";
+        currentNode.dataset.finish = "true";
+    }
+}
+
+function setStart(event) {
+    // Select node that was clicked
+    let currentNode = event.currentTarget;
+
+    if (currentNode.dataset.wall !== "true") {
+        currentNode.style.backgroundColor = "green";
+        currentNode.dataset.start = "true";
+        advanceStage();
+    }
+}
+
 function clearMap() {
-    for (let node of globals.searchDiv.children) {
-        node.dataset.wall = false;
+    for (let node of searchDiv.children) {
+        node.dataset.wall = "false";
         node.style.backgroundColor = null;
     }
 }
 
 function drawGrid() {
-    for (let i=0; i<globals.rowNum; i++) {
-        for (let j=0; j<globals.colNum; j++) {
+    for (let i=0; i<rowNum; i++) {
+        for (let j=0; j<colNum; j++) {
             let node = document.createElement("div");
             node.classList.add("node");
             node.dataset.x = j;
             node.dataset.y = i;
-            node.dataset.wall = false;
+            node.dataset.wall = "false";
 
-            node.style.width = globals.boxSize + "%";
-            node.style.height = globals.boxSize + "%";
-            node.style.outline = "1px solid black";
+            node.style.width = boxSize + "%";
+            node.style.height = boxSize + "%";
 
             node.addEventListener("click", setWall, false);
             node.addEventListener("contextmenu", clearWall, false);
 
-            globals.searchDiv.appendChild(node);
+            searchDiv.appendChild(node);
         }
     }
 }
 
 window.addEventListener("load", drawGrid, false);
-globals.clearButton.addEventListener("click", clearMap, false);
+clearButton.addEventListener("click", clearMap, false);
+startButton.addEventListener("click", advanceStage, false);
